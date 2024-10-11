2f259eced6e7bd1a03407fa9540b0a2b25dc13a5
AlaFadeli <alouli.fadeli@gmail.com>
2024-10-11 17:43:10 +0200

    build: remove assets

--- produce.sh
+++ produce.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 set -e
+rm -r YamlPresentation_assets
 # pip install manim-present
 python -m venv .venv
 source .venv/bin/activate
@@ -7,3 +8,4 @@ npm i -s html-inject-meta
 manim-present
 ./node_modules/html-inject-meta/cli.js < YamlPresentation.html  > index.html
 deactivate
+
