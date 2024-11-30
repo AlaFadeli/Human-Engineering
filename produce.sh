f81e0a28a8ba1100d3a13f5a25af4aa4de4736ae
AlaFadeli <alouli.fadeli@gmail.com>
2024-11-30 12:16:41 +0100

    build

--- produce.sh
+++ produce.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 set -e
-rm -r YamlPresentation_assets
-# pip install manim-present
+# rm -r YamlPresentation_assets
 python -m venv .venv
 source .venv/bin/activate
+pip install manim-present==0.0.8
 npm i -s html-inject-meta
 manim-present
 ./node_modules/html-inject-meta/cli.js < YamlPresentation.html  > index.html
