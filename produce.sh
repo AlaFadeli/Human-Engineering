19584c69f5a3c7adc1266cfe0223db67fbee354e
AlaFadeli <alouli.fadeli@gmail.com>
2024-12-04 18:50:01 +0100

    LAST

--- produce.sh
+++ produce.sh
@@ -3,7 +3,7 @@ set -e
 # rm -r YamlPresentation_assets
 python -m venv .venv
 source .venv/bin/activate
-pip install manim-present==0.0.8
+pip install manim-present==0.0.9
 npm i -s html-inject-meta
 manim-present
 ./node_modules/html-inject-meta/cli.js < YamlPresentation.html  > index.html
